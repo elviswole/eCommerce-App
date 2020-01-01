63bfb7128cc9f41d231c9e4fa0ffb13056245c20
Benson Oluwole Elvis <elviswole@yahoo.com>
2020-01-01 22:39:47 +0100

    Building an eCommerce.

---
+++ app/src/main/java/com/example/ecommerceapp/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.ecommerceapp.Prevalent;
+
+import com.example.ecommerceapp.Model.Users;
+
+public class Prevalent
+{
+
+    private  static Users curentUOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
